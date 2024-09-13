a8d44b27945f28ba131633691849778c1b2d1731
Koruda <158458944+Crissdavi@users.noreply.github.com>
2024-09-13 10:13:15 -0500

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -95,6 +95,8 @@ export async function handler(chatUpdate) {
                     chat.nsfw = false
                 if (!isNumber(chat.expired))
                     chat.expired = 0
+                if (!('modoadmin' in chat)) 
+                    chat.modoadmin = false;
             } else
                 global.db.data.chats[m.chat] = {
                     isBanned: false,
@@ -103,6 +105,7 @@ export async function handler(chatUpdate) {
                     onlyLatinos: false,
                     nsfw: false, 
                     expired: 0, 
+                   modoadmin:true,
                 }
             var settings = global.db.data.settings[this.user.jid]
             if (typeof settings !== 'object') global.db.data.settings[this.user.jid] = {}
