1a1f9a721954b143b134b4b76fcff57692861d58
Koruda <158458944+Crissdavi@users.noreply.github.com>
2024-09-28 10:36:16 -0500

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -174,7 +174,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
     let pp14 = 'https://tinyurl.com/ymlqb6ml'
     let pp15 = 'https://tinyurl.com/ykv7g4zy'
     let img = await (await fetch(`https://i.ibb.co/RyVfBHv/file.jpg`)).buffer()
-    await m.react('⭐')
+    await m.react('😼')
    // await conn.sendMessage(m.chat, { video: { url: [pp, pp2, pp3, pp4, pp5, pp6, pp7, pp8, pp9, pp10, pp11, pp12, pp13, pp14, pp15].getRandom() }, gifPlayback: true, caption: text.trim(), mentions: [m.sender] }, { quoted: estilo })
     await conn.sendFile(m.chat, img, 'thumbnail.jpg', text.trim(), m, null, rcanal)
    //await conn.sendAi(m.chat, botname, textbot, text.trim(), img, img, canal, estilo)
