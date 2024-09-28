43d7802424e04b2655d09039655ab88a48bcc094
Koruda <158458944+Crissdavi@users.noreply.github.com>
2024-09-28 10:30:49 -0500

    Update group-hidetag.js

--- plugins/group-hidetag.js
+++ plugins/group-hidetag.js
@@ -37,7 +37,7 @@ const handler = async (m, {conn, text, participants, isOwner, isAdmin}) => {
   }
 };
 handler.help = ['hidetag'];
-handler.tags = ['grupo'];
+handler.tags = ['groups'];
 handler.command = ['hidetag', 'notificar', 'notify'];
 handler.group = true;
 handler.admin = true;
