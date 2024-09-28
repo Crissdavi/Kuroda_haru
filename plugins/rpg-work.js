956878fbefaa34bafef5d461a45f5ab1bffa46bd
Koruda <158458944+Crissdavi@users.noreply.github.com>
2024-09-28 08:47:45 -0500

    Update rpg-work.js

--- plugins/rpg-work.js
+++ plugins/rpg-work.js
@@ -10,7 +10,7 @@ let user = global.db.data.users[m.sender]
   }
   let resultado = Math.floor(Math.random() * 5000)
   cooldowns[m.sender] = Date.now()
-  await conn.reply(m.chat, `🚩 ${pickRandom(works)} *${toNum(resultado)}* ( *${resultado}* ) XP 💫.`, m, rcanal)
+  await conn.reply(m.chat, `🚩 ${pickRandom(works)} *${toNum(resultado)}* ( *${resultado}* ) Zenis 💴.`, m, rcanal)
   user.exp += resultado
 }
 
