af396cde23f4da0c9e6cc599e304156f765b9465
Koruda <158458944+Crissdavi@users.noreply.github.com>
2024-09-28 21:30:47 -0500

    Update game-casino.js

--- plugins/game-casino.js
+++ plugins/game-casino.js
@@ -25,20 +25,20 @@ let handler = async (m, { conn, args, usedPrefix, command, DevMode }) => {
 	    
         count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].limit / buatall) : parseInt(count) : args[0] ? parseInt(args[0]) : 1
         count = Math.max(1, count)
-        if (args.length < 1) return conn.reply(m.chat, '🚩 Ingresa la cantidad de ' + `*⭐ Estrellas*` + ' que deseas aportar contra' + ` *Ai Hoshino*` + `\n\n` + '`Ejemplo:`\n' + `> *${usedPrefix + command}* 100`, m, rcanal)
+        if (args.length < 1) return conn.reply(m.chat, '🚩 Ingresa la cantidad de ' + `*💴 Zenis*` + ' que deseas aportar contra' + ` *Kuroda*` + `\n\n` + '`Ejemplo:`\n' + `> *${usedPrefix + command}* 100`, m, rcanal)
 
         if (user.limit >= count * 1) {
             user.limit -= count * 1
             if (Aku > Kamu) {
-                conn.reply(m.chat, '`🌵 Veamos que numeros tienen!`\n\n'+ `➠ *Ai Hoshino* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username}, *PERDISTE* ${formatNumber(count)} ⭐ Estrellas.`.trim(), m, rcanal)
+                conn.reply(m.chat, '`🌵 Veamos que numeros tienen!`\n\n'+ `➠ *Kuroda* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username}, *PERDISTE* ${formatNumber(count)} 💴 Zenis.`.trim(), m, rcanal)
             } else if (Aku < Kamu) {
                 user.limit += count * 2
-                conn.reply(m.chat, '`🌵 Veamos que numeros tienen!`\n\n'+ `➠ *Ai Hoshino* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username}, *GANASTE* ${formatNumber(count * 2)} ⭐ Estrellas.`.trim(), m, rcanal)
+                conn.reply(m.chat, '`🌵 Veamos que numeros tienen!`\n\n'+ `➠ *Kuroda* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username}, *GANASTE* ${formatNumber(count * 2)} 💴 Zenis.`.trim(), m, rcanal)
             } else {
                 user.limit += count * 1
-                conn.reply(m.chat, '`🌵 Veamos que numeros tienen!`\n\n'+ `➠ *Ai Hoshino* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username} obtienes ${formatNumber(count * 1)} ⭐ Estrellas.`.trim(), m, rcanal)
+                conn.reply(m.chat, '`🌵 Veamos que numeros tienen!`\n\n'+ `➠ *Kuroda* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username} obtienes ${formatNumber(count * 1)} 💴 Zenis.`.trim(), m, rcanal)
             }
-        } else conn.reply(m.chat, `No tienes *${formatNumber(count)} ⭐ Estrellas* para apostar!`.trim(), m, rcanal)
+        } else conn.reply(m.chat, `No tienes *${formatNumber(count)} 💴 Zenis* para apostar!`.trim(), m, rcanal)
     
 }
     
