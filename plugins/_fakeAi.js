4b77b26d33cb68d85ec90ac6982c74837c949722
Koruda <158458944+Crissdavi@users.noreply.github.com>
2024-10-03 21:04:27 -0500

    Update _fakeAi.js

--- plugins/_fakeAi.js
+++ plugins/_fakeAi.js
@@ -14,7 +14,7 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
           "groupJid": "6285240750713-1610340626@g.us",
           "inviteCode": "mememteeeekkeke",
           "groupName": "P",
-          "caption": "Hello, I'm Ai Hoshino",
+          "caption": "Hello, I'm Kuroda",
           "jpegThumbnail": await (await fetch(pp)).buffer()
         }
       }
