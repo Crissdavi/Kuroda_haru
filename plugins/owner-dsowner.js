dfe3a1ddbad0528e968df1d5cf73331074466ab8
Koruda <158458944+Crissdavi@users.noreply.github.com>
2024-10-05 14:32:11 -0500

    Update owner-dsowner.js

--- plugins/owner-dsowner.js
+++ plugins/owner-dsowner.js
@@ -3,13 +3,13 @@ import path from 'path';
 
 const handler = async (m, { conn, usedPrefix }) => {
   if (global.conn.user.jid !== conn.user.jid) {
-    return conn.sendMessage(m.chat, {text: '⚠️ Este comando solo puede ser utilizado en el bot principal de 孫ՏᴏɴᏀᴏᴋᴜ孫.'}, {quoted: m});
+    return conn.sendMessage(m.chat, {text: '⚠️ Este comando solo puede ser utilizado en el bot principal de Kuroda.'}, {quoted: m});
   }
   await conn.sendMessage(m.chat, {text: '✅ Iniciando...'}, {quoted: m});
   const sessionPath = './sessions'
   try {
     if (!existsSync(sessionPath)) {
-      return await conn.sendMessage(m.chat, {text: '⚠️ La carpeta session de 孫ՏᴏɴᏀᴏᴋᴜ孫 no existe o está vacía.'}, {quoted: m});
+      return await conn.sendMessage(m.chat, {text: '⚠️ La carpeta session de Kuroda no existe o está vacía.'}, {quoted: m});
     }
     const files = await fs.readdir(sessionPath);
     let filesDeleted = 0;
@@ -20,17 +20,17 @@ const handler = async (m, { conn, usedPrefix }) => {
       }
     }
     if (filesDeleted === 0) {
-      await conn.sendMessage(m.chat, {text: '⚠️ No se encontró ningún archivo  de ՏᴏɴᏀᴏᴋᴜ para eliminar en la carpeta sessions.'}, {quoted: m});
+      await conn.sendMessage(m.chat, {text: '⚠️ No se encontró ningún archivo  de Kuroda para eliminar en la carpeta sessions.'}, {quoted: m});
     } else {
-      await conn.sendMessage(m.chat, {text: `✅ Se eliminaron ${filesDeleted} archivos de ՏᴏɴᏀᴏᴋᴜ.`}, {quoted: m});
+      await conn.sendMessage(m.chat, {text: `✅ Se eliminaron ${filesDeleted} archivos de Kuroda.`}, {quoted: m});
     }
   } catch {
-    await conn.sendMessage(m.chat, {text: '⚠️ Ocurrió un error al eliminar los archivos de sesión de ՏᴏɴᏀᴏᴋᴜ.'}, {quoted: m});
+    await conn.sendMessage(m.chat, {text: '⚠️ Ocurrió un error al eliminar los archivos de sesión de Kuroda.'}, {quoted: m});
   }
-  await conn.sendMessage(m.chat, {text: `¡Hola! ¿Ahora me ves Att ՏᴏɴᏀᴏᴋᴜ?`}, {quoted: m});
+  await conn.sendMessage(m.chat, {text: `¡Hola! ¿Ahora me ves Att Kuroda?`}, {quoted: m});
 };
 handler.tags = ['owner']
-handler.help = ['dsowner', 'ds']
+handler.help = ['dsowner']
 handler.command = /^(del_reg_in_session_owner|dsowner|clearallsession)$/i;
 handler.rowner = true
 export default handler;
