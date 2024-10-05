ca0b1c80859cfc53d9ec44cbe16a54207ea15833
Koruda <158458944+Crissdavi@users.noreply.github.com>
2024-10-05 08:55:34 -0500

    Update search-tiktok.js

--- plugins/search-tiktok.js
+++ plugins/search-tiktok.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import Starlights from '@StarlightsTeam/Scraper'
 
 let handler = async (m, { conn, usedPrefix, command, text, args }) => {
-  if (!text) return conn.reply(m.chat, `🚩 Ingresa el nombre video que deseas buscar en TikTok.\n\nEjemplo:\n> *${usedPrefix + command}* Ai Hoshino Edit`, m, rcanal)
+  if (!text) return conn.reply(m.chat, `🚩 Ingresa el nombre video que deseas buscar en TikTok.\n\nEjemplo:\n> *${usedPrefix + command}* lady k and the sick man edit`, m, rcanal)
   
   await m.react('🕓')
   let img = await (await axios.get('https://i.ibb.co/BTCtjgG/file.jpg', { responseType: 'arraybuffer' })).data
