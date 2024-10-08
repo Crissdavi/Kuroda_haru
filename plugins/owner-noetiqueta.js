3dcb21f3c5ff3ce2054162a8e3f8bfcb6ae56714
Koruda <158458944+Crissdavi@users.noreply.github.com>
2024-10-08 17:29:59 +0200

    Actualizar owner-noetiqueta.js

--- plugins/owner-noetiqueta.js
+++ plugins/owner-noetiqueta.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn, usedPrefix, isOwner }) => {
 await m.react('😺')
 await conn.reply(m.chat, `Hola @${m.sender.split`@`[0]} si necesitas la ayuda de mi creador porfavor escribele al privado\n*- Solo asuntos importantes -*`, estilo, { mentions: [m.sender] })
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;おDanịel.xyz⁩;;\nFN:おDanịel.xyz⁩\nORG:おDanịel.xyz⁩\nTITLE:\nitem1.TEL;waid=5218261000681:5218261000681\nitem1.X-ABLabel:おDanịel.xyz⁩\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:おDanịel.xyz⁩\nEND:VCARD`
-await conn.sendMessage(m.chat, { contacts: { displayName: 'おDanịel.xyz⁩', contacts: [{ vcard }] }}, {quoted: m})
+let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;Criss;;\nFN:Criss\nORG:Criss\nTITLE:\nitem1.TEL;waid=5218261000681:5218261000681\nitem1.X-ABLabel:Criss\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:Criss\nEND:VCARD`
+await conn.sendMessage(m.chat, { contacts: { displayName: 'Criss', contacts: [{ vcard }] }}, {quoted: m})
 }
-handler.customPrefix = /^(@5218261275256|@5218132588591|@5218139760662|@5215659171599)$/i
+handler.customPrefix = /^(@51913776697)$/i
 handler.command = new RegExp
 export default handler
