8b0fad904bb89004953af110bb0f52b06a250f36
Koruda <158458944+Crissdavi@users.noreply.github.com>
2024-10-20 22:55:31 -0500

    Update and rename youtube-ytmp4 (1).js to youtube-ytmp4.js

--- plugins/youtube-ytmp4.js
+++ plugins/youtube-ytmp4.js
@@ -1,6 +1,6 @@
 import Starlights from '@StarlightsTeam/Scraper'
 import fetch from 'node-fetch' 
-let limit = 100
+let limit = 500
 
 let handler = async (m, { conn, args, text, isPrems, isOwner, usedPrefix, command }) => {
 if (!args[0]) return conn.reply(m.chat, '[ ✰ ] Ingresa el enlace del vídeo de *YouTube* junto al comando.\n\n`» Ejemplo :`\n' + `> *${usedPrefix + command}* https://youtu.be/QSvaCSt8ixs`, m, rcanal)
@@ -15,7 +15,7 @@ if (size.split('MB')[0] >= limit) return conn.reply(m.chat, `El archivo pesa mas
        txt += `	✩   *Titulo* : ${title}\n`
        txt += `	✩   *Duración* : ${duration}\n`
        txt += `	✩   *Tamaño* : ${size}\n\n`
-       txt += `> *- ↻ El vídeo se esta enviando espera un momento, soy lenta. . .*`
+       txt += `> *- ↻ El vídeo se esta enviando espera un momento, soy lento. . .*`
 await conn.sendMessage(m.chat, {image: img, caption: txt }, {quoted: m})
 await conn.sendMessage(m.chat, { video: { url: dl_url }, caption: `${title}`, mimetype: 'video/mp4', fileName: `${title}` + `.mp4`}, {quoted: m })
 await m.react('✅')
@@ -29,7 +29,7 @@ if (size.split('MB')[0] >= limit) return conn.reply(m.chat, `El archivo pesa mas
        txt += `	✩   *Titulo* : ${title}\n`
        txt += `	✩   *Calidad* : ${quality}\n`
        txt += `	✩   *Tamaño* : ${size}\n\n`
-       txt += `> *- ↻ El vídeo se esta enviando espera un momento, soy lenta. . .*`
+       txt += `> *- ↻ El vídeo se esta enviando espera un momento, soy lento. . .*`
 await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)
 await conn.sendMessage(m.chat, { video: { url: dl_url }, caption: `${title}`, mimetype: 'video/mp4', fileName: `${title}` + `.mp4`}, {quoted: m })
 await m.react('✅')
