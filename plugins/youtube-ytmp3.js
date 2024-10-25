c1ec8b33e5fe4f8a5be2ca19ea3dc1a32e48498a
Koruda <158458944+Crissdavi@users.noreply.github.com>
2024-10-25 09:11:22 -0500

    Update youtube-ytmp3.js

--- plugins/youtube-ytmp3.js
+++ plugins/youtube-ytmp3.js
@@ -37,9 +37,7 @@ let data_play = await ytdl(videos.url)
 console.log(data_play)
 await conn.sendMessage(m.chat, { 
         audio: { url: data_play.data.mp3 }, 
-    await m.react('✅')
         mimetype: 'audio/mp4', 
-    await m.react('✅')
       }, { quoted: m });
 }
 handler.help = ['ytmp3 <yt url>']
