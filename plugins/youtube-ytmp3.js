e3c24c24740098c169ff6f2eabbfd807ed6aa338
Koruda <158458944+Crissdavi@users.noreply.github.com>
2024-10-25 09:27:30 -0500

    Update youtube-ytmp3.js

--- plugins/youtube-ytmp3.js
+++ plugins/youtube-ytmp3.js
@@ -1,52 +1,57 @@
 import yts from 'yt-search';
 import axios from 'axios';
-import fetch from "node-fetch"
+import fetch from "node-fetch";
 
 const handler = async (m, { text, usedPrefix, command, conn }) => {
     if (!text) {
-      throw m.reply("✧ Ingresa una consulta de *YouTube*");
+        throw m.reply("✧ Ingresa una consulta de *YouTube*");
     }
-    await m.react('🕓')
+    await m.react('🕓');
+    
     let res = await yts(text);
     let videoList = res.all;
     let videos = videoList[0];
 
+    async function ytdl(url) {
+        const response = await fetch('https://shinoa.us.kg/api/download/ytdl', {
+            method: 'POST',
+            headers: {
+                'accept': '*/*',
+                'api_key': 'free',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                text: url
+            })
+        });
 
-async function ytdl(url) {
-    const response = await fetch('https://shinoa.us.kg/api/download/ytdl', {
-        method: 'POST',
-        headers: {
-            'accept': '*/*',
-            'api_key': 'free',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            text: url
-        })
-    });
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
 
-    if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
+        const data = await response.json();
+        return data;
     }
 
-    const data = await response.json();
-    return data;
-}
+    let data_play = await ytdl(videos.url);
+    console.log(data_play);
 
-let data_play = await ytdl(videos.url)
-console.log(data_play)
-await conn.sendMessage(m.chat, { 
-        audio: { url: data_play.data.mp3 }), 
-        mimetype: 'audio/mp4',
-      }, { quoted: m });
-    await m.react('✅')
-} catch {
-await m.react('✖️')
-  }
+    if (data_play && data_play.data && data_play.data.mp3) {
+        await conn.sendMessage(m.chat, { 
+            audio: { url: data_play.data.mp3 }, 
+            mimetype: 'audio/mp4',
+        }, { quoted: m });
+        
+        await m.react('✅'); 
+    } else {
+        await m.reply("❌ No se pudo obtener el audio.");
+        await m.react('❌'); 
+    }
 };
-handler.help = ['ytmp3 <yt url>']
-handler.tags = ['downloader']
-handler.command = ['ytmp3', 'yta']
-handler.register = false
-//handler.limit = 1
-export default handler
+
+handler.help = ['ytmp3 <yt url>'];
+handler.tags = ['downloader'];
+handler.command = ['ytmp3', 'yta'];
+handler.register = false;
+
+export default handler;
