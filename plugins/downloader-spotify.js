0746482434ea7a6e7e4dbbe3ec124154295409c4
Koruda <158458944+Crissdavi@users.noreply.github.com>
2024-10-26 18:43:55 -0500

    Update downloader-spotify.js

--- plugins/downloader-spotify.js
+++ plugins/downloader-spotify.js
@@ -1,89 +1,73 @@
-import pkg from 'sanzy-spotifydl'
-let { downloadTrack, downloadAlbum, search } = pkg
-import fetch from 'node-fetch'
-import pkg2 from 'fluid-spotify.js'
-let { Spotify } = pkg2
+//*`[ SPOTIFY - DL ]`*
+import axios from 'axios'
 
-let handler = async (m, { conn, text }) => {
- if (!text) throw `🚩 Ingresa el enlace de algún Track, PlayList o Álbum de Spotify.`; 
- let isSpotifyUrl = text.match(/^(https:\/\/open\.spotify\.com\/(album|track|playlist)\/[a-zA-Z0-9]+)/i);
- if (!isSpotifyUrl && !text) throw `🚩 Ingresa el enlace de algún Track, Playlist o Álbum de Spotify.`
-let user = global.db.data.users[m.sender]
-await m.react('🕓')
+let delay = ms => new Promise(resolve => setTimeout(resolve, ms))
+
+let handler = async (m, { conn, args }) => {
+if (!args[0]) return m.reply('Ingresa un enlace de spotify')
 try {
-if (isSpotifyUrl) {
-if (isSpotifyUrl[2] === 'album') {
-let album = await downloadAlbum(isSpotifyUrl[0])
-let img = await (await fetch(`${album.metadata.cover}`)).buffer()
-let txt = `*乂  S P O T I F Y  -  D O W N L O A D*\n\n`
-    txt += `	✩  *Album* : ${album.metadata.title}\n`
-    txt += `	✩   *Artista* :${album.metadata.artists}\n`
-    txt += `	✩   *Publicado* : ${album.metadata.releaseDate}\n`   
-    txt += `	✩   *Tracks totales* : ${album.trackList.length}\n\n`   
-    txt += `*- ↻ Los audios se estan enviando espera un momento, soy lento. . .*`
-await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m)
-for (let i = 0; i < album.trackList.length; i++) {
-await conn.sendFile(m.chat, album.trackList[i].audioBuffer, album.trackList[i].metadata.name + '.mp3', null, m, false, { mimetype: 'audio/mpeg', asDocument: user.useDocument })
-await m.react('✅')
-}
-} else if (isSpotifyUrl[2] === 'track') {
-let track = await downloadTrack(isSpotifyUrl[0])
-let dlspoty = track.audioBuffer
-let img = await (await fetch(`${track.imageUrl}`)).buffer()
-let txt = `*乂  S P O T I F Y  -  D O W N L O A D*\n\n`
-    txt += `	✩   *Título* : ${track.title}\n`
-    txt += `	✩   *Artista* : ${track.artists}\n`
-    txt += `	✩   *Duración* : ${track.duration}\n`
-    txt += `	✩   *Album* : ${track.album.name}\n`                 
-    txt += `	✩   *Publicado* : ${track.album.releasedDate}\n\n`   
-    txt += `*- ↻ El audio se esta enviando espera un momento, soy lento. . .*`
-await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m)
-await conn.sendFile(m.chat, dlspoty, track.title + '.mp3', null, m, false, { mimetype: 'audio/mpeg', asDocument: user.useDocument })
-await m.react('✅')
-} else if (isSpotifyUrl[2] === 'playlist') {
-let infos = new Spotify({
-clientID: "7fb26a02133d463da465671222b9f19b",
-clientSecret: "d4e6f8668f414bb6a668cc5c94079ca1",
-})
-let playlistId = isSpotifyUrl[0].split('/').pop()
-let playlistInfoByID = await infos.getPlaylist(playlistId)
-let tracks = playlistInfoByID.tracks.items
-let img = await (await fetch(`${playlistInfoByID.images[0].url}`)).buffer()
-let txt = `*乂  S P O T I F Y  -  D O W N L O A D*\n\n`
-    txt += `	✩   *Playlist* : ${playlistInfoByID.name}\n`
-    txt += `	✩   *Tracks totales* : ${tracks.length}\n\n`
-    txt += `*- ↻ Los audios se estan enviando espera un momento, soy lento. . .*`
-await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m)
-let target = m.chat
-if (m.isGroup && tracks.length > 20) {
-target = m.sender;
-}
+let api = await axios.get(`https://api.ryzendesu.vip/api/downloader/spotify?url=${encodeURIComponent(args[0])}`)
+let json = api.data
+
+if (json.success) {
+if (json.metadata.playlistName) {
+let playlistName = json.metadata.playlistName
+let cover = json.metadata.cover
+let tracks = json.tracks
+m.reply(`*Playlist:* ${playlistName}
+*Cover:* ${cover}
+*Total Tracks:* ${tracks.length}`)
+
 for (let i = 0; i < tracks.length; i++) {
-let track = await downloadTrack(tracks[i].track.external_urls.spotify)
-await conn.sendFile(m.chat, track.audioBuffer, tracks[i].track.name + '.mp3', null, m, false, { mimetype: 'audio/mpeg', asDocument: user.useDocument })
-await m.react('✅')
-}}
+let track = tracks[i]
+if (track.success) {
+let { title, artists, album, cover, releaseDate } = track.metadata
+let link = track.link  
+let audioGet = await axios.get(link, { responseType: 'arraybuffer' })
+let audio = audioGet.data
+
+await conn.sendMessage(m.chat, {
+document: audio, 
+mimetype: 'audio/mpeg',
+fileName: `${title}.mp3`,
+caption: `
+*Title:* ${title}
+*Artists:* ${artists}
+*Album:* ${album}
+*Release Date:* ${releaseDate}
+*Cover:* ${cover}
+`,
+}, { quoted: m })
+
+await delay(1500)
+} else {}
+}
 } else {
-let searchTrack = await downloadTrack(text)
-let dlspoty = searchTrack.audioBuffer
-let img = await (await fetch(`${searchTrack.imageUrl}`)).buffer()
-let txt = `*乂  S P O T I F Y  -  D O W N L O A D*\n\n`
-    txt += `	✩   *Título* : ${searchTrack.title}\n`
-    txt += `	✩   *Artista* : ${searchTrack.artists}\n`
-    txt += `	✩   *Duración* : ${searchTrack.duration}\n`
-    txt += `	✩   *Album* : ${searchTrack.album.name}\n`                 
-    txt += `	✩   *Publicado* : ${searchTrack.album.releasedDate}\n\n`   
-    txt += `*- ↻ El audio se esta enviando espera un momento, soy lentalo. . .*`
-await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m)
-await conn.sendFile(m.chat, dlspoty, searchTrack.title + '.mp3', null, m, false, { mimetype: 'audio/mpeg', asDocument: user.useDocument })
-await m.react('✅')
-}  
-} catch {
-await m.react('✖️')
+let { title, artists, album, cover, releaseDate } = json.metadata
+let link = json.link  
+
+let audioGet = await axios.get(link, { responseType: 'arraybuffer' })
+let audio = audioGet.data
+
+await conn.sendMessage(m.chat, {
+document: audio,
+mimetype: 'audio/mpeg',
+fileName: `${title}.mp3`,
+caption: `
+*Title:* ${title}
+*Artists:* ${artists}
+*Album:* ${album}
+*Release Date:* ${releaseDate}
+*Cover:* ${cover}
+`,
+}, { quoted: m })
+}
+} else {}
+} catch (error) {
+console.error(error)
 }}
-handler.tags = ['downloader']
-handler.help = ['spotify']
-handler.command = ['spotify']
-//handler.limit = 1
-handler.register = false
+
+
+handler.command = /^(spotifydl)$/i
+
 export default handler
