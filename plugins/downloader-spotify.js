a300c215077b342e274ea5ec8876502c5be3a82b
Koruda <158458944+Crissdavi@users.noreply.github.com>
2024-10-26 18:26:56 -0500

    Update downloader-spotify.js

--- plugins/downloader-spotify.js
+++ plugins/downloader-spotify.js
@@ -1,89 +1,55 @@
-import pkg from 'sanzy-spotifydl'
-let { downloadTrack, downloadAlbum, search } = pkg
-import fetch from 'node-fetch'
-import pkg2 from 'fluid-spotify.js'
-let { Spotify } = pkg2
+import fetch from 'node-fetch';
+import Starlights from '@StarlightsTeam/Scraper';
+import { getDevice } from '@whiskeysockets/baileys';
 
-let handler = async (m, { conn, text }) => {
- if (!text) throw `🚩 Ingresa el enlace de algún Track, PlayList o Álbum de Spotify.`; 
- let isSpotifyUrl = text.match(/^(https:\/\/open\.spotify\.com\/(album|track|playlist)\/[a-zA-Z0-9]+)/i);
- if (!isSpotifyUrl && !text) throw `🚩 Ingresa el enlace de algún Track, Playlist o Álbum de Spotify.`
-let user = global.db.data.users[m.sender]
-await m.react('🕓')
-try {
-if (isSpotifyUrl) {
-if (isSpotifyUrl[2] === 'album') {
-let album = await downloadAlbum(isSpotifyUrl[0])
-let img = await (await fetch(`${album.metadata.cover}`)).buffer()
-let txt = `*乂  S P O T I F Y  -  D O W N L O A D*\n\n`
-    txt += `	✩  *Album* : ${album.metadata.title}\n`
-    txt += `	✩   *Artista* :${album.metadata.artists}\n`
-    txt += `	✩   *Publicado* : ${album.metadata.releaseDate}\n`   
-    txt += `	✩   *Tracks totales* : ${album.trackList.length}\n\n`   
-    txt += `*- ↻ Los audios se estan enviando espera un momento, soy lento. . .*`
-await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m)
-for (let i = 0; i < album.trackList.length; i++) {
-await conn.sendFile(m.chat, album.trackList[i].audioBuffer, album.trackList[i].metadata.name + '.mp3', null, m, false, { mimetype: 'audio/mpeg', asDocument: user.useDocument })
-await m.react('✅')
+let handler = async (m, { conn, command, text, usedPrefix }) => {
+  if (!text) return conn.reply(m.chat, 'Ingresa el texto de lo que quieras buscar', m, rcanal);
+
+  await m.react('🕓'); 
+
+  const deviceType = await getDevice(m.key.id);
+  try {
+    let results = await Starlights.spotifySearch(text);
+    if (!results || results.length === 0) return conn.reply(m.chat, 'No se encontraron resultados', m);
+
+    
+    const firstTrack = results[0];
+
+    
+    let listSections = [];
+    let txt = '*S P O T I F Y - S E A R C H* \n';
+    txt += `*✨ Primer Resultado:*\n`;
+    txt += `*🎵 Título:* ${firstTrack.title}\n`;
+    txt += `*🎤 Artista:* ${firstTrack.artist}\n`;
+    txt += `*💬 Url:* ${firstTrack.url}\n\n`;
+    
+    for (let i = 1; i < (results.length >= 30 ? 30 : results.length); i++) {
+      const track = results[i];
+      listSections.push({
+        title: `Canción Nro ${i + 1}`,
+        rows: [
+          {
+            header: '',
+            title: `${track.title}\n`,
+            description: `Artista: ${track.artist}`,
+            id: `${usedPrefix}spotifydl ${track.url}`
+          }
+        ]
+      });
+    }
+
+    
+    await conn.sendList(m.chat, txt + '* Tema kuroda*', '', 'Mas Resultados', 'https://qu.ax/fPmDc.jpg', listSections, m);
+    await m.react('✅'); 
+  } catch (error) {
+    console.error(error);
+    await m.react('❌'); 
+  }
 }
-} else if (isSpotifyUrl[2] === 'track') {
-let track = await downloadTrack(isSpotifyUrl[0])
-let dlspoty = track.audioBuffer
-let img = await (await fetch(`${track.imageUrl}`)).buffer()
-let txt = `*乂  S P O T I F Y  -  D O W N L O A D*\n\n`
-    txt += `	✩   *Título* : ${track.title}\n`
-    txt += `	✩   *Artista* : ${track.artists}\n`
-    txt += `	✩   *Duración* : ${track.duration}\n`
-    txt += `	✩   *Album* : ${track.album.name}\n`                 
-    txt += `	✩   *Publicado* : ${track.album.releasedDate}\n\n`   
-    txt += `*- ↻ El audio se esta enviando espera un momento, soy lento. . .*`
-await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m)
-await conn.sendFile(m.chat, dlspoty, track.title + '.mp3', null, m, false, { mimetype: 'audio/mpeg', asDocument: user.useDocument })
-await m.react('✅')
-} else if (isSpotifyUrl[2] === 'playlist') {
-let infos = new Spotify({
-clientID: "7fb26a02133d463da465671222b9f19b",
-clientSecret: "d4e6f8668f414bb6a668cc5c94079ca1",
-})
-let playlistId = isSpotifyUrl[0].split('/').pop()
-let playlistInfoByID = await infos.getPlaylist(playlistId)
-let tracks = playlistInfoByID.tracks.items
-let img = await (await fetch(`${playlistInfoByID.images[0].url}`)).buffer()
-let txt = `*乂  S P O T I F Y  -  D O W N L O A D*\n\n`
-    txt += `	✩   *Playlist* : ${playlistInfoByID.name}\n`
-    txt += `	✩   *Tracks totales* : ${tracks.length}\n\n`
-    txt += `*- ↻ Los audios se estan enviando espera un momento, soy lento. . .*`
-await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m)
-let target = m.chat
-if (m.isGroup && tracks.length > 20) {
-target = m.sender;
-}
-for (let i = 0; i < tracks.length; i++) {
-let track = await downloadTrack(tracks[i].track.external_urls.spotify)
-await conn.sendFile(m.chat, track.audioBuffer, tracks[i].track.name + '.mp3', null, m, false, { mimetype: 'audio/mpeg', asDocument: user.useDocument })
-await m.react('✅')
-}}
-} else {
-let searchTrack = await downloadTrack(text)
-let dlspoty = searchTrack.audioBuffer
-let img = await (await fetch(`${searchTrack.imageUrl}`)).buffer()
-let txt = `*乂  S P O T I F Y  -  D O W N L O A D*\n\n`
-    txt += `	✩   *Título* : ${searchTrack.title}\n`
-    txt += `	✩   *Artista* : ${searchTrack.artists}\n`
-    txt += `	✩   *Duración* : ${searchTrack.duration}\n`
-    txt += `	✩   *Album* : ${searchTrack.album.name}\n`                 
-    txt += `	✩   *Publicado* : ${searchTrack.album.releasedDate}\n\n`   
-    txt += `*- ↻ El audio se esta enviando espera un momento, soy lentalo. . .*`
-await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m)
-await conn.sendFile(m.chat, dlspoty, searchTrack.title + '.mp3', null, m, false, { mimetype: 'audio/mpeg', asDocument: user.useDocument })
-await m.react('✅')
-}  
-} catch {
-await m.react('✖️')
-}}
-handler.tags = ['downloader']
-handler.help = ['spotify']
-handler.command = ['spotify']
-//handler.limit = 1
-handler.register = false
-export default handler
+
+handler.help = ['spotifysearch *<búsqueda>*'];
+handler.tags = ['search'];
+handler.command = ['spotifysearch'];
+handler.register = true;
+
+export default handler;
