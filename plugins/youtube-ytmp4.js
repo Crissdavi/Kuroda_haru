ed30b27809e515e377de93c3d9bd116c2b8c57cc
Koruda <158458944+Crissdavi@users.noreply.github.com>
2024-10-27 11:02:56 -0500

    Update youtube-ytmp4.js

--- plugins/youtube-ytmp4.js
+++ plugins/youtube-ytmp4.js
@@ -1,72 +1,57 @@
-import fg from 'api-dylux'
-import yts from 'yt-search'
-import { youtubedl, youtubedlv2 } from '@bochilteam/scraper'
-import fetch from 'node-fetch' 
-let limit = 100
+import yts from 'yt-search';
+import axios from 'axios';
+import fetch from "node-fetch";
 
-let handler = async (m, { conn: star, args, text, isPrems, isOwner, usedPrefix, command }) => {
-if (!args || !args[0]) return star.reply(m.chat, ' Ingresa el enlace del vídeo de YouTube junto al comando.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* https://youtu.be/QSvaCSt8ixs`, m, rcanal)
-if (!args[0].match(/youtu/gi)) return star.reply(m.chat, `Verifica que el enlace sea de YouTube.`, m, rcanal).then(_ => m.react('✖️'))
-let q = args[1] || '360p'
+const handler = async (m, { text, usedPrefix, command, conn }) => {
+    if (!text) {
+        throw m.reply("✧ Ingresa una consulta de *YouTube*");
+    }
+    await m.react('🕓');
+    
+    let res = await yts(text);
+    let videoList = res.all;
+    let videos = videoList[0];
 
-await m.react('🕓')
-try {
-let v = args[0]
-let yt = await youtubedl(v).catch(async () => await youtubedlv2(v))
-let dl_url = await yt.video[q].download()
-let title = await yt.title
-let size = await yt.video[q].fileSizeH 
-let thumbnail = await yt.thumbnail
+    async function ytdl(url) {
+        const response = await fetch('https://shinoa.us.kg/api/download/ytdl', {
+            method: 'POST',
+            headers: {
+                'accept': '*/*',
+                'api_key': 'free',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                text: url
+            })
+        });
 
-let img = await (await fetch(`${thumbnail}`)).buffer()
-if (sizeMB.split('MB')[0] >= limit) return star.reply(m.chat, `El archivo pesa mas de ${limit} MB, se canceló la Descarga.`, m, rcanal).then(_ => m.react('✖️'))
-	let txt = '`乂  Y O U T U B E  -  M P 4`\n\n'
-       txt += `	✩   *Titulo* : ${title}\n`
-       txt += `	✩   *Calidad* : ${q}\n`
-       txt += `	✩   *Tamaño* : ${sizeMB}\n\n`
-       txt += `> *- ↻ El video se esta enviando espera un momento, soy lenta. . .*`
-await star.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)
-await star.sendMessage(m.chat, { video: { url: dl_url }, caption: `${title}`, mimetype: 'video/mp4', fileName: `${title}` + `.mp4`}, {quoted: m })
-await m.react('✅')
-} catch {
-try {
-let yt = await fg.ytv(args[0], q)
-let { title, dl_url, size } = yt 
-let vid = (await yts(text)).all[0]
-let { thumbnail, url } = vid
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
 
-let img = await (await fetch(`${vid.thumbnail}`)).buffer()  
-if (size.split('MB')[0] >= limit) return star.reply(m.chat, `El archivo pesa mas de ${limit} MB, se canceló la Descarga.`, m, rcanal).then(_ => m.react('✖️'))
-	let txt = '`乂  Y O U T U B E  -  M P 4`\n\n'
-       txt += `	✩   *Titulo* : ${title}\n`
-       txt += `	✩   *Calidad* : ${q}\n`
-       txt += `	✩   *Tamaño* : ${size}\n\n`
-       txt += `> *- ↻ El video se esta enviando espera un momento, soy lenta. . .*`
-await star.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)
-await star.sendMessage(m.chat, { video: { url: dl_url }, caption: `${title}`, mimetype: 'video/mp4', fileName: `${title}` + `.mp4`}, {quoted: m })
-await m.react('✅')
-} catch {
-try {
-let yt = await fg.ytmp4(args[0], q)
-let { title, size, dl_url, thumb } = yt
+        const data = await response.json();
+        return data;
+    }
 
-let img = await (await fetch(`${thumb}`)).buffer()
-if (size.split('MB')[0] >= limit) return star.reply(m.chat, `El archivo pesa mas de ${limit} MB, se canceló la Descarga.`, m, rcanal).then(_ => m.react('✖️'))
-	let txt = '`乂  Y O U T U B E  -  M P 4`\n\n'
-       txt += `	✩   *Titulo* : ${title}\n`
-       txt += `	✩   *Calidad* : ${q}\n`
-       txt += `	✩   *Tamaño* : ${size}\n\n`
-       txt += `> *- ↻ El video se esta enviando espera un momento, soy lenta. . .*`
-await star.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)
-await star.sendMessage(m.chat, { video: { url: dl_url }, caption: `${title}`, mimetype: 'video/mp4', fileName: `${title}` + `.mp4`}, {quoted: m })
-await m.react('✅')
-} catch {
-await m.react('✖️')
-}}}}
-handler.help = ['ytmp4 *<link yt>*']
-handler.tags = ['downloader']
-handler.command = ['ytmp4', 'ytv', 'yt']
-//handler.limit = 1
-handler.register = true 
+    let data_play = await ytdl(videos.url);
+    console.log(data_play);
 
-export default handler
+    if (data_play && data_play.data && data_play.data.mp4) {
+        await conn.sendMessage(m.chat, { 
+            video: { url: data_play.data.mp4 }, 
+            mimetype: 'video/mp4',
+        }, { quoted: m });
+        
+        await m.react('✅'); 
+    } else {
+        await m.reply("❌ No se pudo obtener el video.");
+        await m.react('❌'); 
+    }
+};
+
+handler.help = ['ytmp4 <yt url>'];
+handler.tags = ['downloader'];
+handler.command = ['ytmp4'];
+handler.register = true;
+
+export default handler;
