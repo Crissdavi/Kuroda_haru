5f7af94034ff2156a7878e1d9fc279874f2e0e36
Koruda <158458944+Crissdavi@users.noreply.github.com>
2024-10-27 15:19:34 -0500

    Update img-imagen.js

--- plugins/img-imagen.js
+++ plugins/img-imagen.js
@@ -1,50 +1,20 @@
-//*`[ IMAGEN SEARCH - CARRUSEL ]`*
-import axios from 'axios';
-const { proto, generateWAMessageFromContent, generateWAMessageContent } = (await import('@whiskeysockets/baileys')).default
+import Starlights from "@StarlightsTeam/Scraper"
 
-let handler = async (m, { conn, text }) => {
-if (!text) return m.reply('Ingresa el texto de lo que quieres buscar');
-
-async function createImage(url) {
-const { imageMessage } = await generateWAMessageContent({image: { url }}, { upload: conn.waUploadToServer })
-return imageMessage
-}
-
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+if (!text) return m.reply('🚩 Ingresa el nombre de la imágen que estas buscando.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* Ai Hoshino Icons`)
+const prohibited = ['caca', 'polla', 'porno', 'porn', 'gore', 'cum', 'semen', 'puta', 'puto', 'culo', 'putita', 'putito','pussy', 'hentai', 'pene', 'coño', 'asesinato', 'zoofilia', 'mia khalifa', 'desnudo', 'desnuda', 'cuca', 'chocha', 'muertos', 'pornhub', 'xnxx', 'xvideos', 'teta', 'vagina', 'marsha may', 'misha cross', 'sexmex', 'furry', 'furro', 'furra', 'xxx', 'rule34', 'panocha', 'pedofilia', 'necrofilia', 'pinga', 'horny', 'ass', 'nude', 'popo', 'nsfw', 'femdom', 'futanari', 'erofeet', 'sexo', 'sex', 'yuri', 'ero', 'ecchi', 'blowjob', 'anal', 'ahegao', 'pija', 'verga', 'trasero', 'violation', 'violacion', 'bdsm', 'cachonda', '+18', 'cp', 'mia marin', 'lana rhoades', 'cogiendo', 'cepesito', 'hot', 'buceta', 'xxx', 'rule', 'r u l e']
+if (prohibited.some(word => m.text.toLowerCase().includes(word))) return m.reply('Deja de buscar eso puto enfermo de mierda, que por eso no tienes novia.').then(_ => m.react('✖️'))
+await m.react('🕓')
 try {
-let HasumiBotFreeCodes = []
-let { data } = await axios.get(`https://api.ryzendesu.vip/api/search/gimage?query=${encodeURIComponent(text)}`)
-let JT = data
-let imgs = JT.slice(0, 7)
-
-for (let result of imgs) {
-HasumiBotFreeCodes.push({
-header: proto.Message.InteractiveMessage.Header.fromObject({ title: ``, hasMediaAttachment: true, imageMessage: await createImage(result) }),
-body: proto.Message.InteractiveMessage.Body.fromObject({ text: `${text}` }),
-footer: proto.Message.InteractiveMessage.Footer.fromObject({ text: `` }),
-nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.fromObject({ buttons: [] })
-})
-}
-
-const msg = generateWAMessageFromContent(m.chat, {
-viewOnceMessage: {
-message: {
-messageContextInfo: {
-deviceListMetadata: {},
-deviceListMetadataVersion: 2
-},
-interactiveMessage: proto.Message.InteractiveMessage.fromObject({contextInfo: {mentionedJid: [m.sender]},
-body: proto.Message.InteractiveMessage.Body.create({ text: 'HasumiBotFreeCodes' }),
-footer: proto.Message.InteractiveMessage.Footer.create({ text: 'imagen search' }),
-header: proto.Message.InteractiveMessage.Header.create({ hasMediaAttachment: false }),
-carouselMessage: proto.Message.InteractiveMessage.CarouselMessage.fromObject({ cards: [...HasumiBotFreeCodes] })
-})
-}}}, { quoted: m })
-
-await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id })
-} catch (error) {
-console.error(error) 
+let { dl_url } = await Starlights.GoogleImage(text)
+await conn.sendFile(m.chat, dl_url, 'thumbnail.jpg', `*» Resultado* : ${text}`, m, null, rcanal)
+await m.react('✅')
+} catch {
+await m.react('✖️')
 }}
-
-handler.command = ['imagen']
-
+handler.help = ['imagen *<búsqueda>*']
+handler.tags = ['img']
+handler.command = ['image', 'gimage', 'imagen']
+handler.register = true 
+//handler.limit = 1
 export default handler
