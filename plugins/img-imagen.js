85bb059961be101a6ac7ba6d81d54c7677af308f
Koruda <158458944+Crissdavi@users.noreply.github.com>
2024-10-27 15:17:33 -0500

    Update img-imagen.js

--- plugins/img-imagen.js
+++ plugins/img-imagen.js
@@ -1,11 +1,10 @@
-
-
 //*`[ IMAGEN SEARCH - CARRUSEL ]`*
 import axios from 'axios';
 const { proto, generateWAMessageFromContent, generateWAMessageContent } = (await import('@whiskeysockets/baileys')).default
 
 let handler = async (m, { conn, text }) => {
 if (!text) return m.reply('Ingresa el texto de lo que quieres buscar');
+
 async function createImage(url) {
 const { imageMessage } = await generateWAMessageContent({image: { url }}, { upload: conn.waUploadToServer })
 return imageMessage
@@ -20,7 +19,6 @@ let imgs = JT.slice(0, 7)
 for (let result of imgs) {
 HasumiBotFreeCodes.push({
 header: proto.Message.InteractiveMessage.Header.fromObject({ title: ``, hasMediaAttachment: true, imageMessage: await createImage(result) }),
-}}
 body: proto.Message.InteractiveMessage.Body.fromObject({ text: `${text}` }),
 footer: proto.Message.InteractiveMessage.Footer.fromObject({ text: `` }),
 nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.fromObject({ buttons: [] })
@@ -35,10 +33,10 @@ deviceListMetadata: {},
 deviceListMetadataVersion: 2
 },
 interactiveMessage: proto.Message.InteractiveMessage.fromObject({contextInfo: {mentionedJid: [m.sender]},
-body: proto.Message.InteractiveMessage.Body.create({ text: 'Sigue el canal Koruda' }),
+body: proto.Message.InteractiveMessage.Body.create({ text: 'HasumiBotFreeCodes' }),
 footer: proto.Message.InteractiveMessage.Footer.create({ text: 'imagen search' }),
 header: proto.Message.InteractiveMessage.Header.create({ hasMediaAttachment: false }),
-carouselMessage: proto.Message.InteractiveMessage.CarouselMessage.fromObject({ cards: [... Kuroda] })
+carouselMessage: proto.Message.InteractiveMessage.CarouselMessage.fromObject({ cards: [...HasumiBotFreeCodes] })
 })
 }}}, { quoted: m })
 
@@ -46,9 +44,7 @@ await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id })
 } catch (error) {
 console.error(error) 
 }}
-handler.help = ['imagen *<búsqueda>*']
-handler.tags = ['img']
-handler.command = ['imagen', 'img']
-handler.register = true 
-//handler.limit = 1
+
+handler.command = ['imagen']
+
 export default handler
