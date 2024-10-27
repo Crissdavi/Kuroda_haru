bfb77e2290a10a3670903d144d4fd42977333ff0
Koruda <158458944+Crissdavi@users.noreply.github.com>
2024-10-27 07:03:31 -0500

    Add files via upload

---
+++ plugins/downloader-tiktok.js
@@ -0,0 +1,24 @@
+
+//*`[ TIKTOK - DL ]`*
+import axios from 'axios'
+
+let handler = async (m, { conn, args }) => {
+if (!args[0]) return m.reply('https://vm.tiktok.com/ZMhNNeDHU/')
+try {
+let api = await axios.get(`https://api.ryzendesu.vip/api/downloader/ttdl?url=${encodeURIComponent(args[0])}`)
+let json = api.data
+let { data, processed_time:proceso } = json
+let { play:video, duration:duracion, title: titulo, music:audio } = data
+
+let JT = `*Titulo:* ${titulo}
+*Proceso:* ${proceso} ms
+*Duracion:* ${duracion}`
+await conn.sendFile(m.chat, video, 'HasumiBotFreeCodes.mp4', JT, m)
+await conn.sendFile(m.chat, audio, 'HasumiBotFreeCodes.mp3', null, m)
+} catch (error) {
+console.error(error)    
+}}
+
+handler.command = /^(tiktok)$/i
+
+export default handler
