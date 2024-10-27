51d33e673c27538030229386e3cc0f9d7f6ef176
Koruda <158458944+Crissdavi@users.noreply.github.com>
2024-10-27 10:46:07 -0500

    Actualizar youtube-ytmp4.js

--- plugins/youtube-ytmp4.js
+++ plugins/youtube-ytmp4.js
@@ -1,54 +1,24 @@
-import fg from 'api-dylux'
-import yts from 'yt-search'
-import { youtubedl, youtubedlv2 } from '@bochilteam/scraper'
-let limit = 500
+import fetch from 'node-fetch';
 
-let handler = async (m, { conn, args, text, isPrems, isOwner, usedPrefix, command }) => {
-if (!args || !args[0]) return conn.reply(m.chat, `🐢 Escribe la URL de un video de YouTube que deseas descargar.`, m)
-if (!args[0].match(/youtu/gi)) return conn.reply(m.chat,`Verifica que la *URL* sea de YouTube`, m).then(_ => m.react('✖️'))
-let q = args[1] || '720p'
-
-await m.react('🕓')
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+if (!args[0]) return conn.reply(m.chat, '*Ingresa un enlace de youtube*', m);
+ 
 try {
-const yt = await fg.ytv(args[0], q)
-let { title, dl_url, size } = yt 
-let vid = (await yts(text)).all[0]
+let api = await fetch(`https://apis-starlights-team.koyeb.app/starlight/ytmp4?url=${args[0]}`);
+let json = await api.json();
+let { id, thumbnails, url, download } = json
 
-if (size.split('MB')[500] >= limit) return conn.reply(m.chat, `El archivo pesa mas de ${limit} MB, se canceló la Descarga.`, m).then(_ => m.react('✖️'))
+let txt = `*ID* : ${id}
+*Url* : ${url}`;
+await conn.sendFile(m.chat, thumbnails.high, 'ytmp4.jpg', txt, m)
 
-await conn.sendMessage(m.chat, {
-        text: ` *Título ∙* ${title}\n⚖️ *Tamaño ∙* ${size}\n\n*↻ Espera @${m.sender.split`@`[0]}, se está enviando su video en 720P. . .*`,
-        contextInfo: { 
-          mentionedJid: [m.sender],
-        }
-      }, { quoted: m })
+await conn.sendMessage(m.chat, { video: { url: download }, caption: null, mimetype: 'video/mp4', fileName: `${id}` + `.mp4`}, {quoted: m })
 
-await conn.sendFile(m.chat, dl_url, 'yt.jpg', `${vid.title}\n⇆ㅤㅤ◁ㅤㅤ❚❚ㅤㅤ▷ㅤㅤ↻\n Duración ${vid.timestamp}`, m)
-await m.react('✅')
 } catch {
-try {
-let yt = await fg.ytmp4(args[0], q)
-let { title, size, dl_url } = yt
-let vid = (await yts(text)).all[0]
+conn.reply('error :v')    
+}
+}
+    
+handler.command = ['ytmp4'];
 
-if (size.split('MB')[500] >= limit) return conn.reply(m.chat, `El archivo pesa mas de ${limit} MB, se canceló la Descarga.`, m).then(_ => m.react('✖️'))
-
-await conn.sendMessage(m.chat, {
-        text: ` *Título ∙* ${title}\n⚖️ *Tamaño ∙* ${size}\n\n*↻ Espera @${m.sender.split`@`[0]},se  está enviando su video . . .*`,
-        contextInfo: { 
-          mentionedJid: [m.sender],
-        }
-      }, { quoted: m })
-
-await conn.sendFile(m.chat, dl_url, 'yt.jpg', `${vid.title}\n⇆ㅤㅤ◁ㅤㅤ❚❚ㅤㅤ▷ㅤㅤ↻\n Duración ${vid.timestamp}`, m)
-await m.react('✅')
-} catch {
-await conn.reply(m.chat,`*☓ Ocurrió un error inesperado pesa más de 500MB use el comando ytmp4doc para descargar *`, m).then(_ => m.react('✖️'))
-//console.error(error)
-}}}
-handler.help = ['ytmp4 <yt url>']
-handler.tags = ['downloader']
-handler.command = ['ytmp4', 'yt', 'ytv']
-handler.register = true 
-//handler.limit = 1
-export default handler
+export default handler;
