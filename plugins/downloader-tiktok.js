bb72ecfeeda54e0ae7d450f41dee9ca37a06f929
Koruda <158458944+Crissdavi@users.noreply.github.com>
2024-11-01 15:17:04 -0500

    Actualizar downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -9,9 +9,17 @@ let json = api.data
 let { data, processed_time:proceso } = json
 let { play:video, duration:duracion, title: titulo, music:audio } = data
 
-let JT = `*Titulo:* ${titulo}
-*Proceso:* ${proceso} ms
-*Duracion:* ${duracion}`
+let txt = '`乂  T I K T O K  -  D O W N L O A D`\n\n'
+    txt += `	✩  *Título* : ${title}\n`
+    txt += `	✩  *Autor* : ${author}\n`
+    txt += `	✩  *Duración* : ${duration} segundos\n`
+    txt += `	✩  *Vistas* : ${views}\n`
+    txt += `	✩  *Likes* : ${likes}\n`
+    txt += `	✩  *Comentarios* : ${comment}\n`
+    txt += `	✩  *Compartidos* : ${share}\n`
+    txt += `	✩  *Publicado* : ${published}\n`
+    txt += `	✩  *Descargas* : ${downloads}\n\n`
+    txt += `> 🚩 *${textbot}*`
 await conn.sendFile(m.chat, video, 'HasumiBotFreeCodes.mp4', JT, m)
 await conn.sendFile(m.chat, audio, 'HasumiBotFreeCodes.mp3', null, m)
 } catch (error) {
