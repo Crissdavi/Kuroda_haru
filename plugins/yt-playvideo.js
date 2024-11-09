1b96658e26ed37293d49bfac2308cbacfb703777
Koruda <158458944+Crissdavi@users.noreply.github.com>
2024-11-09 09:07:32 -0500

    Update yt-playvideo.js

--- plugins/yt-playvideo.js
+++ plugins/yt-playvideo.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 import yts from 'yt-search'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) throw m.reply(`Ingresa una consulta\n*✧ Ejemplo:* ${usedPrefix}${command} Waguri Edit`);
+if (!text) throw m.reply(`Ingrese su consulta\n* Ejemplo:* ${usedPrefix}${command} billie jean`);
 conn.sendMessage(m.chat, { react: { text: "🕒", key: m.key } });
     let results = await yts(text);
     let tes = results.all[0]
@@ -16,7 +16,7 @@ conn.sendMessage(m.chat, { react: { text: "🕒", key: m.key } });
     } = tes;
   let d2 = await fetch(`https://exonity.tech/api/ytdlp2-faster?apikey=adminsepuh&url=${url}`)
   let dp = await d2.json()
-  m.reply(`_✧ Enviando su video en formato mp4
+  m.reply(`_ Enviando su video
   ${dp.result.title} (${dp.result.duration})_\n\n> ${url}`)
     
 const getBuffer = async (url) => {
@@ -25,8 +25,8 @@ const getBuffer = async (url) => {
     const buffer = await response.arrayBuffer();
     return Buffer.from(buffer);
   } catch (error) {
-    console.error("Error al obtener el buffer", error);
-    throw new Error("Error al obtener el buffer");
+    console.error("Error al obtener el video", error);
+    throw new Error("Error al obtener el vídeo");
   }
 }
     let videop = await getBuffer(dp.result.media.mp4)
