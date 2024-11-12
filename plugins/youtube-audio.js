cb5fa11c33e2c379977fab642fcada6a21dde839
Koruda <158458944+Crissdavi@users.noreply.github.com>
2024-11-11 23:35:01 -0500

    Update youtube-audio.js

--- plugins/youtube-audio.js
+++ plugins/youtube-audio.js
@@ -25,7 +25,7 @@ let { title, size, quality, thumbnail, dl_url } = await Starlights.ytmp3(v)
 
 if (size.split('MB')[0] >= limit) return m.reply(`El archivo pesa mas de ${limit} MB, se canceló la Descarga.`).then(_ => m.react('✖️'))
 
-await conn.sendFile(m.chat, dl_url, title + '.mp3', null, m, false, { mimetype: 'audio/mpeg', asDocument: user.useDocument })
+await conn.sendFile(m.chat, dl_url, title + '.mp3', null, m, false, { mimetype: 'audio/mp3', asDocument: user.useDocument })
 await m.react('✅')
 } catch {
 await m.react('✖️')
