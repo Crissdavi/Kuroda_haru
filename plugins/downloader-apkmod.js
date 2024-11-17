2e54a6b8c3ebf57c0e801f6bbeb6e6127b757a98
Koruda <158458944+Crissdavi@users.noreply.github.com>
2024-11-17 01:13:26 -0500

    Actualizar downloader-apkmod.js

--- plugins/downloader-apkmod.js
+++ plugins/downloader-apkmod.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, args, usedPrefix, command, isOwner }) => {
-if (!args[0]) throw `\`\`\`[ 🌟 ] Ingresa el nombre de la aplicación que quieres descargar. Ejemplo:\n${usedPrefix + command} SonGoku\`\`\``
+if (!args[0]) throw `\`\`\`[ 🌟 ] Ingresa el nombre de la aplicación que quieres descargar. Ejemplo:\n${usedPrefix + command} Koruda\`\`\``
 let res = await fetch(`https://api.dorratz.com/v2/apk-dl?text=${args[0]}`);
 let result = await res.json();
 let { name, size, lastUpdate, icon } = result;
