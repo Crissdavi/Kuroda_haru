8c4a29bf03de18fc5736a93cd359acb3c807e8b0
Koruda <158458944+Crissdavi@users.noreply.github.com>
2024-11-24 21:43:44 -0500

    Update fun-revealgender.js

--- plugins/fun-revealgender.js
+++ plugins/fun-revealgender.js
@@ -1,7 +1,8 @@
 let handler = async (m, { command, text }) => m.reply(`
 *🚫 𝐑𝐞𝐯𝐞𝐥𝐚𝐧𝐝𝐨 𝐠é𝐧𝐞𝐫𝐨 🚫*
   
-*𝙿𝚁𝙴𝙶𝚄𝙽𝚃𝙰:* ${text}
+*𝐐𝐮𝐞 𝐠𝐞𝐧𝐞𝐫ó 𝐞𝐬
+:* ${text}
 *𝚁𝙴𝚂𝙿𝚄𝙴𝚂𝚃𝙰:* ${['Mujer','Hombre','trans','Bisexual','Polisexual', 'Pansexual', 'Ni idea'].getRandom()}
 `.trim(), null, m.mentionedJid ? {
 mentions: m.mentionedJid
