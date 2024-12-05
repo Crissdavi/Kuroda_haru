cdfb05519c7273780a73cdb8a8224c83141d6ac1
Koruda <158458944+Crissdavi@users.noreply.github.com>
2024-12-05 11:48:24 -0500

    Actualizar rg-perfil.js

--- plugins/rg-perfil.js
+++ plugins/rg-perfil.js
@@ -26,7 +26,7 @@ let handler = async (m, { conn, usedPrefix, command}) => {
       txt += `│  ✩  *Numero* : ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}\n`
       txt += `│  ✩  *Nacionalidad* : ${userNationality}\n`
       txt += `│  ✩  *Link* : wa.me/${who.split`@`[0]}\n`
-      txt += `│  ✩  *Casad@:* ${isMarried ? partnerName : 'Nadie'}`
+      txt += `│  ✩  *Casad@*: ${isMarried ? partnerName : 'Nadie'}`
       txt += `│  ✩  *Zenis* : ${limit}\n`
       txt += `│  ✩  *Nivel* : ${level}\n`
       txt += `│  ✩  *XP* : Total ${exp} (${user.exp - min}/${xp})\n`
