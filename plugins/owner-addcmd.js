5004fe0d0ef4f9f6ea4cf77d853beadc76cfe9e6
Koruda <158458944+Crissdavi@users.noreply.github.com>
2024-12-08 11:05:41 -0500

    Update owner-addcmd.js

--- plugins/owner-addcmd.js
+++ plugins/owner-addcmd.js
@@ -1,7 +1,7 @@
 const handler = async (m, {conn, text, usedPrefix, command}) => {
   global.db.data.sticker = global.db.data.sticker || {};
   if (!m.quoted) throw `*Responde a un archivo / sticker / imágen*`;
-  if (!m.quoted.fileSha256) throw `*Responde a un sticker pije*`;
+  if (!m.quoted.fileSha256) throw `*Responde a un sticker *`;
   if (!text) throw `*Añade un texto o comando:*\n*—◉ ${usedPrefix + command} #kick*\n\n*Ejemplo de uso:*\n*—◉ ${usedPrefix + command} <#menu> <sticker>*`;
   const sticker = global.db.data.sticker;
   const hash = m.quoted.fileSha256.toString('base64');
