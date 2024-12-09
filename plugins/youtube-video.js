d3b888066935d8c00c3a40e2e9708afdeea0f0a1
Koruda <158458944+Crissdavi@users.noreply.github.com>
2024-12-09 14:26:23 -0500

    Create youtube-video.js

---
+++ plugins/youtube-video.js
@@ -0,0 +1,39 @@
+import Starlights from '@StarlightsTeam/Scraper'
+let limit = 1000
+
+let handler = async (m, { conn, text, isPrems, isOwner, usedPrefix, command }) => {
+if (!m.quoted) return conn.reply(m.chat, `[ ✰ ] Etiqueta el mensaje que contenga el resultado de YouTube Play.`, m, rcanal).then(_ => m.react('✖️'))
+if (!m.quoted.text.includes("乂  Y O U T U B E  -  P L A Y")) return conn.reply(m.chat, `[ ✰ ] Etiqueta el mensaje que contenga el resultado de YouTube Play.`, m, rcanal).then(_ => m.react('✖️'))
+let urls = m.quoted.text.match(new RegExp(/(?:https?:\/\/)?(?:youtu\.be\/|(?:www\.|m\.)?youtube\.com\/(?:watch|v|embed|shorts)(?:\.php)?(?:\?.*v=|\/))([a-zA-Z0-9\_-]+)/, 'gi'))
+if (!urls) return conn.reply(m.chat, `Resultado no Encontrado.`, m, rcanal).then(_ => m.react('✖️'))
+if (urls.length < text) return conn.reply(m.chat, `Resultado no Encontrado.`, m, rcanal).then(_ => m.react('✖️'))
+let user = global.db.data.users[m.sender]
+
+await m.react('🕓')
+try {
+let v = urls[0]
+let { title, duration, size, thumbnail, dl_url } = await Starlights.ytmp4v2(v)
+
+if (size.split('MB')[0] >= limit) return conn.reply(m.chat, `El archivo pesa mas de ${limit} MB, se canceló la Descarga.`, m, rcanal).then(_ => m.react('✖️'))
+
+await conn.sendFile(m.chat, dl_url, title + '.mp4', `*» Título* : ${title}`, m, false, { asDocument: user.useDocument })
+await m.react('✅')
+} catch {
+try {
+let v = urls[0]
+let { title, size, quality, thumbnail, dl_url } = await Starlights.ytmp4(v)
+
+if (size.split('MB')[0] >= limit) return m.reply(`El archivo pesa mas de ${limit} MB, se canceló la Descarga.`).then(_ => m.react('✖️'))
+
+await conn.sendFile(m.chat, dl_url, title + '.mp4', `*» Título* : ${title}\n*» Calidad* : ${quality}`, m, false, { asDocument: user.useDocument })
+await m.react('✅')
+} catch {
+await m.react('✖️')
+}}}
+handler.help = ['Video']
+handler.tags = ['youtube']
+handler.customPrefix = /^(Video|video|vídeo|Vídeo)/
+handler.command = new RegExp
+//handler.limit = 2
+
+export default handler
