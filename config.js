51fe41550dc28abb0c1378a9f547c4761c936b1b
Koruda <158458944+Crissdavi@users.noreply.github.com>
2024-12-14 21:42:22 -0500

    Actualizar config.js

--- config.js
+++ config.js
@@ -12,9 +12,10 @@ import axios from 'axios'
 global.owner = [
   ['51913776697', 'ㅤㅤㅤㅤㅤ…⁠ᘛDaniel', true],
   ['51910026998', 'bot', true],
+  ['522431268546','Tesis, true],
 
 ]
-global.fkontak = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
+
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
