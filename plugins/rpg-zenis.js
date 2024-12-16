f96ffba1099c779706b5f8d0ecbf7bcf03d941d8
Koruda <158458944+Crissdavi@users.noreply.github.com>
2024-12-16 07:34:14 -0500

    Actualizar rpg-zenis.js

--- plugins/rpg-zenis.js
+++ plugins/rpg-zenis.js
@@ -9,7 +9,7 @@ let handler = async (m, {conn, usedPrefix}) => {
     let img = await (await fetch(`https://i.ibb.co/Lh2qsjy/file.jpg`)).buffer()
     let txt = ` –  *S T A R S  -  U S E R*\n\n`
         txt += `┌  ✩  *🪪 Nombre* : ${user.name}\n`
-        txt += `│  ✩  *💴 Zenis* : ${toNum(user.limit)} ( *${user.limit}* )\n`
+        txt += `│  ✩  *💴 Zenis* : ${toNum(user.zenis)} ( *${user.zenis}* )\n`
         txt += `│  ✩  *🏦 Bank* : ${toNum(user.bank)} ( *${user.bank}* )\n`
         txt += `└  ✩  *💫 XP* : ${toNum(user.exp)} ( *${user.exp}* )`
     let mentionedJid = [who]
