9e61c9b92956208da064d8ec0f171290147dced2
Koruda <158458944+Crissdavi@users.noreply.github.com>
2024-12-16 01:26:33 -0500

    Actualizar fun-slut.js

--- plugins/fun-slut.js
+++ plugins/fun-slut.js
@@ -11,7 +11,7 @@ let user = global.db.data.users[m.sender]
   let resultado = Math.floor(Math.random() * 2500)
   cooldowns[m.sender] = Date.now()
   await conn.reply(m.chat, ` ${pickRandom(works)} *${toNum(resultado)}* ( *${resultado}* ) *💴 Zenis* .`, m, rcanal)
-  user.limit += resultado
+  user.zenis += resultado
 }
 
 handler.help = ['slut']
