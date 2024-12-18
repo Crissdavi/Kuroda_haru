952901423e3d86b15933c50b1905036e863d3b37
Koruda <158458944+Crissdavi@users.noreply.github.com>
2024-12-18 01:07:54 -0500

    Update rpg-lb.js

--- plugins/rpg-lb.js
+++ plugins/rpg-lb.js
@@ -9,7 +9,7 @@ let usersLim = sortedLim.map(enumGetKey)
 let usersLevel = sortedLevel.map(enumGetKey)
 let len = args[0] && args[0].length > 0 ? Math.min(5, Math.max(parseInt(args[0]), 5)) : Math.min(5, sortedExp.length)
 let text = `
-╭───═[ *Top ${len}  Zenis 💴* ]═────⋆
+╭───═[ *Top ${zenis}  Zenis 💴* ]═────⋆
 │╭───────────────···
 ✩│ Tú eres el *${usersLim.indexOf(m.sender) + 1}* de *${usersLim.length}*
 ✩│ ${sortedLim.slice(0, len).map(({ jid, zenis }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *${zenis} 💴*`).join`\n✩│ `}
