41c3ced0d9d808c476fe8eaff14c82a146e0a5eb
Koruda <158458944+Crissdavi@users.noreply.github.com>
2024-12-19 15:34:08 -0500

    Update youtube-ytmp3.js

--- plugins/youtube-ytmp3.js
+++ plugins/youtube-ytmp3.js
@@ -1,1 +1,19 @@
 
+// *[ ❀ YTMP3 ]*
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+if (!text) return conn.reply(m.chat, `❀ Ingresa un  link de youtube`, m)
+    
+try {
+let api = await (await fetch(`https://api.siputzx.my.id/api/d/ytmp3?url=${text}`)).json()
+let dl_url = api.data.dl
+
+conn.sendMessage(m.chat, { audio: { url: dl_url }, mimetype: "audio/mp4", ptt: true }, { quoted: m })
+} catch (error) {
+console.error(error)
+}}
+
+handler.command = ['ytmp3']
+
+export default handler
