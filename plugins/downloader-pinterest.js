1261a79e52f389d7edbd161bd6ee732520682739
Koruda <158458944+Crissdavi@users.noreply.github.com>
2024-12-21 07:41:09 -0500

    Actualizar downloader-pinterest.js

--- plugins/downloader-pinterest.js
+++ plugins/downloader-pinterest.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 if (!text) throw m.reply(`Ingresa un link de pinterest\n*✧ Ejemplo:* ${usedPrefix}${command} https://pin.it/7I5UODZJB`);
 conn.sendMessage(m.chat, { react: { text: "🕒", key: m.key } });
-	let ouh = await fetch(`https://api.agatz.xyz/api/pinterest?url=${text}`)
+	let ouh = await fetch(`https://api.zenkey.my.id/api/download/pinterest?q=loli&apikey=?url=${text}`)
   let gyh = await ouh.json()
 	await conn.sendFile(m.chat, gyh.data.result, `pinvideobykeni.mp4`, `*✧ Url:* ${gyh.data.url}`, m)
 	await conn.sendMessage(m.chat, { react: { text: '✅', key: m.key }})
