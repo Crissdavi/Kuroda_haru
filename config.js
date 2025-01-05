de0eaa8699d9c0f4ddf312cfbe9579546da233d9
Koruda <158458944+Crissdavi@users.noreply.github.com>
2025-01-05 17:16:41 -0500

    Actualizar config.js

--- config.js
+++ config.js
@@ -53,6 +53,7 @@ global.cheerio = cheerio
 global.fs = fs
 global.fetch = fetch
 global.axios = axios
+global.jadi = serbot
 //global.moment = moment	
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
