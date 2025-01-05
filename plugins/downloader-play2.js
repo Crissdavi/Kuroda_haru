de69e26c4373f376156f41d907ee87b4ac384805
Koruda <158458944+Crissdavi@users.noreply.github.com>
2025-01-05 15:38:00 -0500

    Actualizar downloader-play2.js

--- plugins/downloader-play2.js
+++ plugins/downloader-play2.js
@@ -20,9 +20,9 @@ await m.react('✅')
 } catch {
 await m.react('✖️')
 }}
-handler.help = ['play *<búsqueda>*']
+handler.help = ['play2 *<búsqueda>*']
 handler.tags = ['downloader']
-handler.command = ['play']
+handler.command = ['play2']
 handler.register = true 
 export default handler
 
