41a89fe12bf6ff2868cbe32b6def1d360bfff4d6
Koruda <158458944+Crissdavi@users.noreply.github.com>
2025-01-06 09:12:17 -0500

    Actualizar plugins

---
+++ plugins/search-pixiv.js
@@ -0,0 +1,24 @@
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, command, text, usedPrefix }) => {
+if (!text) return conn.reply(m.chat, `❀ Ingresa el texto de lo que quieres buscar`, m)
+
+try {
+let api = await fetch(`https://api.vreden.web.id/api/pixiv-r18?query=${text}`)
+let json = await api.json()
+if (!json.result) {
+return conn.reply(m.chat, '❀ Sin resultados', m)
+}
+let res = json.result[Math.floor(Math.random() * json.result.length)]
+if (!res || !res.title || !res.urls || !res.urls.regular) {
+return conn.reply(m.chat, '❀ Sin resultados', m)
+}
+let { title, urls } = res
+await conn.sendFile(m.chat, urls.regular, 'HasumiBotFreeCodes.jpg', title, m)
+} catch (error) {
+console.error(error)
+}}
+
+handler.command = /^(pixivsearch)$/i
+
+export default handler
