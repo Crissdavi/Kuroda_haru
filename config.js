f54b3b1fdc36fa244116fe3b57e150765b10775c
Koruda <158458944+Crissdavi@users.noreply.github.com>
2025-01-05 23:18:57 -0500

    Update config.js

--- config.js
+++ config.js
@@ -53,7 +53,6 @@ global.cheerio = cheerio
 global.fs = fs
 global.fetch = fetch
 global.axios = axios
-global.jadi = serbot
 //global.moment = moment	
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
