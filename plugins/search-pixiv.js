147de2e02eab2c084f8360d83a2f69575bd667cd
Koruda <158458944+Crissdavi@users.noreply.github.com>
2025-01-06 11:51:03 -0500

    Actualizar search-pixiv.js

--- plugins/search-pixiv.js
+++ plugins/search-pixiv.js
@@ -1,20 +1,20 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, command, text, usedPrefix }) => {
-if (!text) return conn.reply(m.chat, `❀ Ingresa el texto de lo que quieres buscar`, m, null, rcanal)
+if (!text) return conn.reply(m.chat, `❀ Ingresa el texto de lo que quieres buscar`, m,rcanal)
 
 try {
 let api = await fetch(`https://api.vreden.web.id/api/pixiv-r18?query=${text}`)
 let json = await api.json()
 if (!json.result) {
-return conn.reply(m.chat, '❀ Sin resultados', m, null, rcanal)
+return conn.reply(m.chat, '❀ Sin resultados', m,rcanal)
 }
 let res = json.result[Math.floor(Math.random() * json.result.length)]
 if (!res || !res.title || !res.urls || !res.urls.regular) {
-return conn.reply(m.chat, '❀ Sin resultados', m, null, rcanal)
+return conn.reply(m.chat, '❀ Sin resultados', m,rcanal)
 }
 let { title, urls } = res
-await conn.sendFile(m.chat, urls.regular, 'HasumiBotFreeCodes.jpg', title, m, null, rcanal)
+await conn.sendFile(m.chat, urls.regular, 'HasumiBotFreeCodes.jpg', title, m,rcanal)
 } catch (error) {
 console.error(error)
 }}
