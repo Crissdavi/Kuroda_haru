26b9cf1fe03d24af5df9e245cb760245061ff566
Koruda <158458944+Crissdavi@users.noreply.github.com>
2025-01-16 22:07:26 -0500

    Actualizar downloader-ig.js

--- plugins/downloader-ig.js
+++ plugins/downloader-ig.js
@@ -1,27 +1,24 @@
-import fetch from 'node-fetch'
 
-let handler = async (m, { conn, usedPrefix, command, args }) => {
-if (!args[0]) return m.reply(`Ingresa un enlace de Instagram`)
-    
+import axios from 'axios'
+
+let handler = async (m, { conn, usedPrefix, args, command, text }) => {
+if (!args[0]) return conn.reply(m.chat, `❀ Ingresa un link de Instagram`, m)
+
+if (!args[0].match(new RegExp('^https?:\\/\\/(www\\.)?instagram\\.com\\/(p|tv|reel)\\/([a-zA-Z0-9_-]+)(\\/)?(\\?.*)?$'))) return conn.reply(m.chat, ` Verifica que sea un link de instagram`, m)
+
 try {
-let api = await fetch(`https://deliriussapi-oficial.vercel.app/download/instagram?url=${args[0]}`)
-let json = await api.json()
-let { data } = json
-let JT = data
-for (let i = 0; i < JT.length; i++) {
-let HFC = JT[i];
-if (HFC.type === "image") {
-await conn.sendMessage(m.chat, { image: { url: HFC.url } }, { quoted: m })
-} else if (HFC.type === "video") {
-await conn.sendMessage(m.chat, { video: { url: HFC.url } }, { quoted: m })
+let api = await axios.get(`https://apidl.asepharyana.cloud/api/downloader/igdl?url=${args[0]}`)
+for (let a of api.data.data) {
+if (a.url.includes('jpg') || a.url.includes('png') || a.url.includes('jpeg') || a.url.includes('webp') || a.url.includes('heic') || a.url.includes('tiff') || a.url.includes('bmp')) {
+await conn.sendMessage(m.chat, { image: { url: a.url } }, { quoted: m })
+} else {
+await conn.sendMessage(m.chat, { video: { url: a.url } }, { quoted: m })
 }}
 } catch (error) {
-console.error(error)
+console.log(error)
 }}
-handler.tags = ['downloader']
-handler.help = ['ig']
-handler.command = /^(igdl|ig|instagramdl|instagram)$/i
+
+
+handler.command = /^(instagramdl|igdl|ig|instagram)$/i
 
 export default handler
-
-
