b982b3409ab38a029cbd9b5d7da8d2be516fc182
Koruda <158458944+Crissdavi@users.noreply.github.com>
2025-01-18 07:37:06 -0500

    Actualizar youtube-ytmp3.js

--- plugins/youtube-ytmp3.js
+++ plugins/youtube-ytmp3.js
@@ -1,19 +1,25 @@
-
-// *[ ❀ YTMP3 ]*
 import fetch from 'node-fetch'
 
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) return conn.reply(m.chat, `❀ Ingresa un  link de youtube`, m)
-    
-try {
-let api = await (await fetch(`https://api.siputzx.my.id/api/d/ytmp3?url=${text}`)).json()
-let dl_url = api.data.dl
+let handler = async (m, { conn, command, text, usedPrefix }) => {
+if (!text) return conn.reply(m.chat, `❀ Ingresa un link de youtube`, m)
 
-conn.sendMessage(m.chat, { audio: { url: dl_url }, mimetype: "audio/mp4", ptt: true }, { quoted: m })
+try {
+let api = await fetch(`https://axeel.my.id/api/download/audio?url=${text}`)
+let json = await api.json()
+let { title, views, likes, description, author } = json.metadata
+let HS = `- *Titulo :* ${title}
+- *Descripcion :* ${description}
+- *Visitas :* ${views}
+- *Likes :* ${likes}
+- *Autor :* ${author}
+- *Tamaño :* ${json.downloads.size}
+`
+m.reply(HS)
+await conn.sendMessage(m.chat, { audio: { url: json.downloads.url }, mimetype: 'audio/mpeg' }, { quoted: m });
 } catch (error) {
 console.error(error)
 }}
 
-handler.command = ['ytmp3']
+handler.command = /^(ytmp3)$/i
 
 export default handler
