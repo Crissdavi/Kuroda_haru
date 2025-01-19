5454a42f33e7ebc02f2a559b102f921c809ba5ce
Koruda <158458944+Crissdavi@users.noreply.github.com>
2025-01-18 22:15:00 -0500

    Actualizar youtube-ytmp4.js

--- plugins/youtube-ytmp4.js
+++ plugins/youtube-ytmp4.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) return conn.reply(m.chat, `❀ Ingresa un  link de youtube`, m)
+if (!text) return conn.reply(m.chat, `🪐 Ingresa un  link de youtube`, m)
 
 try {
 await m.react('🕒');
