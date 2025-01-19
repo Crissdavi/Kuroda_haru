9dc2a894fabbc5014bf961198cce16150c522ab7
Koruda <158458944+Crissdavi@users.noreply.github.com>
2025-01-19 10:50:55 -0500

    Actualizar tools-ia.js

--- plugins/tools-ia.js
+++ plugins/tools-ia.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) return conn.reply(m.chat, `❀ Ingresa un texto para hablar con chatgpt`, m, null, rcanal)
+if (!text) return conn.reply(m.chat, `🪐 Ingresa un texto para hablar con chatgpt`, m, null, rcanal)
 
 
 try {
