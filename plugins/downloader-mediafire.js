0c0ac36a5f6bf9e2d1508696b04c0dd5281922c6
Koruda <158458944+Crissdavi@users.noreply.github.com>
2025-01-27 20:11:29 -0500

    Actualizar downloader-mediafire.js

--- plugins/downloader-mediafire.js
+++ plugins/downloader-mediafire.js
@@ -1,27 +1,24 @@
-import Starlights from "@StarlightsTeam/Scraper"
+import fetch from 'node-fetch';
 
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!args[0]) return conn.reply(m.chat, '🚩 Ingrese el enlace de un archivo de Mediafire.', m, rcanal)
-if (!args[0].match(/mediafire/gi)) return conn.reply(m.chat, 'El enlace deve ser de un archivo de Mediafire.', m, rcanal)
-await m.react('🕓')
-try {
-let { title, ext, aploud, size, dl_url } = await Starlights.mediafire(args[0])
-let txt = `乂  *M E D I A F I R E  -  D O W N L O A D*\n\n`
-    txt += `	✩  *Nombre* : ${title}\n`
-    txt += `	✩  *Peso* : ${size}\n`
-    txt += `	✩  *Publicado* : ${aploud}\n`
-    txt += `	✩  *MimeType* : ${ext}\n\n`
-    txt += `*- ↻ El archivo se esta enviando espera un momento, soy lenta. . .*`
-let img = await (await fetch('https://i.ibb.co/wLQFn7q/logo-mediafire.jpg')).buffer()
-await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)
-await conn.sendFile(m.chat, dl_url, title, null, m, null, { mimetype: ext, asDocument: true })
-await m.react('✅')
-} catch {
-await m.react('✖️')
-}}
-handler.help = ['mediafire'].map(v => v + ' *<url>*')
-handler.tags = ['downloader', 'premium']
-handler.command = ['mediafire', 'mdfire', 'mf']
-handler.premium = false
+let handler = async (m, { conn, text }) => {
+    if (!text) return conn.reply(m.chat, `🪐 Ingresa un link de mediafire`, m)
+  await m.react('🕓');
 
-export default handler
+    try {
+        let api = await fetch(`https://restapi.apibotwa.biz.id/api/mediafire?url=${text}`)
+        let json = await api.json()
+        let { filename, type, size, uploaded, ext, mimetype, download: dl_url } = json.data.response
+        m.reply(`🪐 Espera Un Momento, Estamos Enviando Su Pedido `)
+        await m.react('✅');
+        await conn.sendFile(m.chat, dl_url, filename, null, m, null, { mimetype: ext, asDocument: true })
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+handler.help = ['mediafire *<url>*']
+handler.tags = ['descargas']
+handler.command = ['mediafire', 'mf']
+handler.zenis = 20;
+
+export default handler;
