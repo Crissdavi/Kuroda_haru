cc2c490f697f461fbfb336de643b283696fed436
Koruda <158458944+Crissdavi@users.noreply.github.com>
2025-01-28 22:49:50 -0500

    Actualizar downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -22,7 +22,7 @@ await m.react('✅')
 } catch {
 await m.react('✖️')
 }}
-handler.help = ['tiktok *<url tt>*']
+handler.help = ['tiktok *<url tt>*, 'tt', 'ttdl']
 handler.tags = ['downloader']
 handler.command = /^(tiktok|ttdl|tiktokdl|tiktoknowm)$/i
 handler.register = true
