1d06750152acbc3d27896f05cf32f236f4cd35e4
Koruda <158458944+Crissdavi@users.noreply.github.com>
2025-01-29 20:58:37 -0500

    Actualizar youtube-ytmp3.js

--- plugins/youtube-ytmp3.js
+++ plugins/youtube-ytmp3.js
@@ -1,63 +1,17 @@
-import fetch from 'node-fetch';
 
-let handler = async (m, { conn, text }) => {
-  if (!text) {
-    await m.react('✖️');
-    throw '☁️ Ingresa un enlace de YouTube.';
-  }
+import fetch from 'node-fetch'
 
-  const ytRegex = /^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.be)\/.+$/;
-  if (!ytRegex.test(text)) {
-    await m.react('❌');
-    throw '☁️ Ingresa un enlace válido de YouTube.';
-  }
+let HS = async (m, { conn, command, text, usedPrefix }) => {
+if (!text) return conn.reply(m.chat, '🪐 ingresa un link de youtube', m, null, rcanal)
+try {
+let api = await fetch(`https://api.davidcyriltech.my.id/download/ytmp4?url=${text}`)
+let json = await api.json()
+let { title, quality, thumbnail, download_url } = json.result
+await conn.sendMessage(m.chat, { video: { url: download_url }, caption: title }, { quoted: m })
+} catch (error) {
+console.error(error)
+}}
 
-  try {
-    await m.react('🕒');
+HS.command = ['ytmp4']
 
-    let videoId = text.split('v=')[1]?.split('&')[0] || text.split('/').pop();
-    let apiURL = `https://www.youtube.com/oembed?url=https://www.youtube.com/watch?v=${videoId}&format=json`;
-
-    let response = await fetch(apiURL);
-    if (!response.ok) throw 'No se pudo obtener información del video. Verifica la URL proporcionada.';
-
-    let videoData = await response.json();
-
-    let ytData = {
-      url: text,
-      title: videoData.title || 'Sin título',
-      thumbnail: videoData.thumbnail_url || `https://img.youtube.com/vi/${videoId}/0.jpg`
-    };
-
-    await conn.sendMessage(m.chat, {
-      audio: {
-        url: `https://kepolu-ytdl.hf.space/yt/dl?url=${ytData.url}&type=audio`
-      },
-      mimetype: 'audio/mpeg',
-      contextInfo: {
-        externalAdReply: {
-          title: ytData.title,
-          body: '𝐊𝐔𝐑𝐎𝐃𝐀',
-          mediaType: 2,
-          mediaUrl: ytData.url,
-          thumbnailUrl: ytData.thumbnail,
-          sourceUrl: ytData.url,
-          containsAutoReply: true,
-          renderLargerThumbnail: true,
-          showAdAttribution: false,
-        }
-      }
-    }, { quoted: m });
-
-    await m.react('✅');
-  } catch (error) {
-    console.error(error);
-    await m.react('❌');
-    conn.reply(m.chat, 'Ocurrió un error al intentar descargar el audio.', m);
-  }
-};
-
-handler.help = ['ytmp3 *<url>*'];
-handler.command = ['ytmp3'];
-handler.tags = ['youtube'];
-export default handler;
+export default HS
