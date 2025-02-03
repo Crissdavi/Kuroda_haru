cb0b8831fac8e50897c82e7378f6dfdebb9ec52c
Koruda <158458944+Crissdavi@users.noreply.github.com>
2025-02-03 14:43:37 -0500

    Actualizar sticker-sticker.js

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -2,22 +2,20 @@ import { sticker } from '../lib/sticker.js'
 import uploadFile from '../lib/uploadFile.js'
 import uploadImage from '../lib/uploadImage.js'
 import { webp2png } from '../lib/webp2mp4.js'
-
 let handler = async (m, { conn, args, usedPrefix, command }) => {
   let stiker = false
-const usser = conn.getName(m.sender)
-const h = ` ㌹ ${usser}`;
-const i = ``
-  try {         
+  let username = conn.getName(m.sender)
+  try {
+
     let q = m.quoted ? m.quoted : m
     let mime = (q.msg || q).mimetype || q.mediaType || ''
     if (/webp|image|video/g.test(mime)) {
-      if (/video/g.test(mime)) if ((q.msg || q).seconds > 11) return m.reply('Máximo 10 segundos')
+      if (/video/g.test(mime)) if ((q.msg || q).seconds > 11) return m.reply('Máximo *10* segundos')
       let img = await q.download?.()
-      if (!img) return conn.reply(m.chat,`🪐 Responde a una imagen o video con*${usedPrefix + command}*`, m, rcanal)
+      if (!img) return conn.reply(m.chat, `🚩 Responda a una *Imagen* o *Vídeo.*`, m, rcanal)
       let out
       try {
-        stiker = await sticker(img, false, h, i)
+        stiker = await sticker(img, false, global.packname, global.author)
       } catch (e) {
         console.error(e)
       } finally {
@@ -26,24 +24,24 @@ const i = ``
           else if (/image/g.test(mime)) out = await uploadImage(img)
           else if (/video/g.test(mime)) out = await uploadFile(img)
           if (typeof out !== 'string') out = await uploadImage(img)
-          stiker = await sticker(false, out, h, i)
+          stiker = await sticker(false, out, global.packname, global.author)
         }
       }
     } else if (args[0]) {
       if (isUrl(args[0])) stiker = await sticker(false, args[0], global.packname, global.author)
-      else return m.reply('URL invalido')
+      else return m.reply('La *Url* es invalida')
     }
   } catch (e) {
     console.error(e)
     if (!stiker) stiker = e
   } finally {
-    if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, null, rpl)
-    else return m.reply(m.chat, 'Donde está la imagen o el vídeo.?', m, rcanal)
+    if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
+    else return conn.reply(m.chat, '🚩 Responda a una *Imagen* o *Vídeo.*', m, rcanal)
   }
 }
 handler.help = ['sticker']
 handler.tags = ['sticker']
-handler.command = ['s', 'sticker'] 
+handler.command = ['s', 'sticker', 'stiker'] 
 
 export default handler
 
