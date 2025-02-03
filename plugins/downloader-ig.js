6cb652ecdae04da32c959ddd3a5c367c9f93e592
Koruda <158458944+Crissdavi@users.noreply.github.com>
2025-02-03 16:06:15 -0500

    Actualizar downloader-ig.js

--- plugins/downloader-ig.js
+++ plugins/downloader-ig.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 let handler = async (m, { conn, usedPrefix, args, command, text }) => {
     if (!args[0]) {
         await m.react('✖️');
-        return conn.reply(m.chat, `🍟 Ingresa un link de Instagram`, m, fake);
+        return conn.reply(m.chat, `🪐 Ingresa un link de Instagram`, m, fake);
     }
 
     if (!args[0].match(new RegExp('^https?:\\/\\/www\\.instagram\\.com\\/([a-zA-Z0-9_-]+)\\/.*$'))) {
