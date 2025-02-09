54e86877b85d9cf11cdedd899e4d3e94f9ed0e87
Koruda <158458944+Crissdavi@users.noreply.github.com>
2025-02-09 09:23:40 -0500

    Create tools-letramusic.js

---
+++ plugins/tools-letramusic.js
@@ -0,0 +1,29 @@
+import fetch from 'node-fetch';
+
+let handler = async(m, { conn, text, usedPrefix, command }) => {
+
+if (!text) return m.reply(m.chat, '🪐 Ingrese Un Nombre De Alguna Cancion', m, rcanal);
+
+try {
+let api = `https://archive-ui.tanakadomp.biz.id/search/lirik?q=${text}`;
+
+let responde = await fetch(api);
+let json = await responde.json();
+let crow = json.result;
+
+let txt = `*Nombre:* ${crow.title}\n*Letra:* ${crow.lyrics}`;
+
+let img = crow.thumb;
+
+conn.sendMessage(m.chat, { image: { url: img }, caption: txt }, { quoted: fkontak });
+
+} catch (e) {
+console.log(e)
+m.reply('*No se pudo obtener la letra De su canción*');
+m.react('✖️');
+ }
+};
+
+handler.command = ['lyrics', 'letramusic];
+
+export default handler;
