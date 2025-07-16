c181a57cc4929b20345981f695edff39ff5c16e8
Koruda <158458944+Crissdavi@users.noreply.github.com>
2025-07-16 09:08:28 -0500

    Actualizar fun-nalgear.js

--- plugins/fun-nalgear.js
+++ plugins/fun-nalgear.js
@@ -32,7 +32,7 @@ let handler = async (m, { conn, usedPrefix }) => {
 }
 
 handler.help = ['nalguear @tag'];
-handler.tags = ['fun'];
+handler.tags = ['nalguear'];
 handler.command = ['nalguear'];
 handler.register = true;
 handler.group = true;
