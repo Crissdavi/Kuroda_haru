2e78d38f7eefc97b7b3b8c5ccf253de16ceaf134
Koruda <158458944+Crissdavi@users.noreply.github.com>
2025-08-13 07:13:17 -0500

    Actualizar _fakeAi.js

--- plugins/_fakeAi.js
+++ plugins/_fakeAi.js
@@ -26,6 +26,6 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
   }
 }
 
-handler.customPrefix = /^(kuroda)$/i
+handler.customPrefix = /^(kuroda|Kuro)$/i
 handler.command = new RegExp
 export default handler
