10af55dc29e0681abf78387dc63d5f9b7147aece
Koruda <158458944+Crissdavi@users.noreply.github.com>
2025-08-22 20:54:47 -0500

    Actualizar rpg-zenis.js

--- plugins/rpg-zenis.js
+++ plugins/rpg-zenis.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, {conn, usedPrefix}) => {
-	
+
     let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
     let user = global.db.data.users[who]
     let name = conn.getName(who)
@@ -9,11 +9,11 @@ let handler = async (m, {conn, usedPrefix}) => {
     let img = await (await fetch(`https://files.catbox.moe/z1wvch.jpeg`)).buffer()
     let txt = ` –  *S T A R S  -  U S E R*\n\n`
         txt += `┌  ✩  *🪪 Nombre* : ${user.name}\n`
-        txt += `│  ✩  *💴 Zenis* : ${(user.zenis)} ( *${user.zenis}* )\n`
+        txt += `│  ✩  *💴 Zenis* : ${toNum(user.zenis)} ( *${user.zenis}* )\n`
         txt += `│  ✩  *🏦 Bank* : ${toNum(user.bank)} ( *${user.bank}* )\n`
         txt += `└  ✩  *💫 XP* : ${toNum(user.exp)} ( *${user.exp}* )`
     let mentionedJid = [who]
-        
+
 await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)
 }
 handler.help = ['zenis']
@@ -32,6 +32,6 @@ function toNum(number) {
     } else if (number <= -1000000) {
         return (number / 1000000).toFixed(1) + 'M'
     } else {
-        return number.toString()
+        return number
     }
 }
