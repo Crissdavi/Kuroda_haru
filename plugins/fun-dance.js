f0b2bec86d1e658dce6c200acd3543492f6544f9
Koruda <158458944+Crissdavi@users.noreply.github.com>
2025-08-27 16:50:34 -0500

    Actualizar fun-dance.js

--- plugins/fun-dance.js
+++ plugins/fun-dance.js
@@ -1,18 +1,33 @@
-let handler = async (m, { conn, usedPrefix, command}) => {
-let pp = 'https://tinyurl.com/26djysdo'
+import fs from 'fs';
+import path from 'path';
+
+let handler = async (m, { conn, usedPrefix }) => {
+    let who;
+    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false;
+    else who = m.chat;
+    if (!who) throw 'Etiqueta o menciona a alguien';
+
+    let user = global.db.data.users[who];
+    let name = conn.getName(who);
+    let name2 = conn.getName(m.sender);
+    m.react('💃');
+    let str = `${name2} está bailando con ${name}`.trim();
+    if (m.isGroup){
+
+    let pp = 'https://tinyurl.com/26djysdo'
 let pp2 = 'https://tinyurl.com/294oahv9'
 let pp3 = 'https://sylphy.xyz/download/D83L9G.mp4'
 let pp4 = 'https://sylphy.xyz/download/pesgBH.mp4'
-let who
-if (m.isGroup) who = m.mentionedJid[0]
-else who = m.chat
-if (!who) return conn.reply(m.chat, '✦ Menciona al usuario con *@user*', m, rcanal)
-let name2 = conn.getName(who)
-let name = conn.getName(m.sender)
+    const videos = [pp, pp2, pp3];
+    const video = videos[Math.floor(Math.random() * videos.length)];
+    conn.sendMessage(m.chat, { video: { url: video }, gifPlayback: true, caption:str, mentions: [m.sender] },{ quoted: estilo })
+    };
 
-await conn.sendMessage(m.chat, { video: { url: [pp, pp2, pp3, pp4].getRandom() }, gifPlayback: true, caption: `*${name}*` + ' está bailando con' + ` *${name2}*` + ' (ﾉ^ヮ^)ﾉ*:・ﾟ✧' }, { quoted: m })
 }
-handler.help = ['dance *<@user>*']
-handler.tags = ['fun']
-handler.command = ['dance', 'bailar']
-export default handler
+
+handler.help = ['dance @tag'];
+handler.tags = ['fun'];
+handler.command = ['dance','bailar'];
+handler.group = true;
+
+export default handler;
