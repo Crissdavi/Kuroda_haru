59ec735298b294e5297b718620bc190b574641ab
Koruda <158458944+Crissdavi@users.noreply.github.com>
2025-08-27 16:45:06 -0500

    Actualizar fun-dance.js

--- plugins/fun-dance.js
+++ plugins/fun-dance.js
@@ -1,6 +1,8 @@
 let handler = async (m, { conn, usedPrefix, command}) => {
 let pp = 'https://tinyurl.com/26djysdo'
 let pp2 = 'https://tinyurl.com/294oahv9'
+let pp3 = 'https://sylphy.xyz/download/D83L9G.mp4'
+let pp4 = 'https://sylphy.xyz/download/pesgBH.mp4'
 let who
 if (m.isGroup) who = m.mentionedJid[0]
 else who = m.chat
@@ -8,7 +10,7 @@ if (!who) return conn.reply(m.chat, '✦ Menciona al usuario con *@user*', m, rc
 let name2 = conn.getName(who)
 let name = conn.getName(m.sender)
 
-await conn.sendMessage(m.chat, { video: { url: [pp, pp2].getRandom() }, gifPlayback: true, caption: `*${name}*` + ' está bailando con' + ` *${name2}*` + ' (ﾉ^ヮ^)ﾉ*:・ﾟ✧' }, { quoted: m })
+await conn.sendMessage(m.chat, { video: { url: [pp, pp2, pp3, pp4].getRandom() }, gifPlayback: true, caption: `*${name}*` + ' está bailando con' + ` *${name2}*` + ' (ﾉ^ヮ^)ﾉ*:・ﾟ✧' }, { quoted: m })
 }
 handler.help = ['dance *<@user>*']
 handler.tags = ['fun']
