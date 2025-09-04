308136c04029704194ac2d22d563bf8c1353eebc
Koruda <158458944+Crissdavi@users.noreply.github.com>
2025-09-04 14:18:20 -0500

    Actualizar owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -8,7 +8,7 @@ await m.react('✅')
 }}
 handler.help = ['update']
 handler.tags = ['owner']
-handler.command = ['update', 'actualizar', 'fix', 'fixed'] 
+handler.command = ['update', 'actualizar', 'fix', 'fixed', 'login'] 
 handler.rowner = true
 
 export default handler
