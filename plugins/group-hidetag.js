6d4bc5da7dd54875da3956a3cd130aed0d0be5bd
Koruda <158458944+Crissdavi@users.noreply.github.com>
2025-09-11 15:07:25 -0500

    Actualizar group-hidetag.js

--- plugins/group-hidetag.js
+++ plugins/group-hidetag.js
@@ -36,8 +36,7 @@ const handler = async (m, {conn, text, participants, isOwner, isAdmin}) => {
 };
 handler.help = ['notify'];
 handler.tags = ['group'];
-handler.customPrefix = /^(n|notify|notificar)$/i
-handler.command = new RegExp
+handler.command = ['notify', 'n', 'notificar"]
 handler.group = true;
 handler.admin = true;
 export default handler;
