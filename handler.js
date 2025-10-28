aa2b7aea737cc8e192c5a8be243b386ff1a42503
Koruda <158458944+Crissdavi@users.noreply.github.com>
2025-10-27 20:40:52 -0500

    Update handler.js

--- handler.js
+++ handler.js
@@ -206,7 +206,7 @@ const senderNum = normalizeJid(m.sender)
 const botNums = [this.user?.jid, this.user?.lid].filter(Boolean).map(j => normalizeJid(cleanJid(j)))
 
 const user = m.isGroup 
-  ? participants.find(u => normalizeJid(u.id && u.jid) === senderNum) 
+  ? participants.find(u => normalizeJid(u.jid) === senderNum) 
   : {}
 
 const bot = m.isGroup 
