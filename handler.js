1c9372b0256756d4cd769c6cf4beee0f9edbd523
Koruda <158458944+Crissdavi@users.noreply.github.com>
2025-11-09 23:21:23 -0500

    Update handler.js

--- handler.js
+++ handler.js
@@ -210,8 +210,8 @@ const user = m.isGroup
   : {}
 
 const bot = m.isGroup 
-  ? participants.find(u => botNums.includes(normalizeJid(cleanJid(u.phoneNumber)))) 
-  : {} 
+  ? participants.find(u => botNums.includes(normalizeJid(cleanJid(u.jid)))) 
+  : {}
 const isRAdmin = user?.admin === 'superadmin'
 const isAdmin = isRAdmin || user?.admin === 'admin'
 const isBotAdmin = bot?.admin === 'admin' || bot?.admin === 'superadmin'
